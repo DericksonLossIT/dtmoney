8da0d1228a2f65e59f14a5736c4c35cf9c952832
derickson.loss <derickson.loss@paylivre.com>
2021-12-06 22:37:09 -0300

    adiciona styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`;
+
 function App() {
   return (
     <div className="App">
-      <div>Hello world!</div>
+      <Title>Hello World</Title>
     </div>
   );
 }
